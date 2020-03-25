7a584e997c8e36b33ed69a4c729b525cfa075ae6
DarkFathom <emailsteveschmidt@gmail.com>
2020-03-25 12:32:57 -0400

    Adding deployment files

---
+++ startup-script.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+#create project folder
+mkdir /home/project-files
+
+#install node
+#curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
+#sudo yum install nodejs
+
+#install other stuff
